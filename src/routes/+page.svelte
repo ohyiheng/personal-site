53f7e07d56e2f07ecc65817cd4b5aa968c301312
Yi Heng <rainmrn@proton.me>
2024-06-04 17:35:34 +0800

    First commit - basic landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import NavButton from "./NavButton.svelte";
+</script>
+
+<div>
+    <logo>yiheng</logo>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+
+    <NavButton text={"now"} />
+</div>
+
+<style>
+    logo {
+        font-family: var(--font-serif);
+    }
+    div {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: left;
+    }
+</style>
---
+++ src/routes/NavButton.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let text;
+</script>
+
+<button>{text}</button>
+
+<style>
+    button {
+        padding: 1em 5em;
+        color: #fff;
+        background-color: var(--accent);
+        border: none;
+        border-radius: .2rem;
+        font-size: 1rem;
+        font-family: var(--font-sans);
+        font-weight: 500;
+    }
+</style>
