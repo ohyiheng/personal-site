f9ed900f60f160df29405b45a967830e82fe9934
Yi Heng <rainmrn@proton.me>
2024-06-05 00:49:33 +0800

    Add a heading component

---
+++ src/Heading.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let content;
+</script>
+
+<div class="w-4/5 flex justify-center items-center -rotate-2 mx-auto mb-12">
+    <h1 class="text-5xl text-white text-center rotate-2">{content}</h1>
+</div>
+
+<style>
+    div {
+        background-color: var(--accent);
+        height: 100px;
+    }
+</style>
