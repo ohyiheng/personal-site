a1057dc4bce273868398394a43716f5dfff2b7b2
Yi Heng <rainmrn@proton.me>
2024-06-07 11:03:56 +0800

    Update /now page content

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -4,6 +4,28 @@
     export let data;
 </script>
 
+<div class="space-y-4 mb-10">
+    <p>
+        I'm currently on a sembreak, chilling at home. My next semester is going
+        to be a lot harder than the first two, as I'm about to dive into
+        unfamiliar territory, stuff like Discrete Math, Computer Architecture,
+        more advanced Data Structures.
+    </p>
+    <p>
+        They seem exciting, but I'm also worried about them taking up too much
+        of my focus. On one hand I do want to direct most of my focus on the
+        courses, but on the other hand, I would also like to spend more time
+        doing other stuff, maybe joining clubs and societies, or taking up a
+        personal project.
+    </p>
+    <p class="italic">
+        Last updated: 2024-06-06.<br /> This page is inspired by the
+        <a href="https://nownownow.com/about" target="_blank"
+            >/now page movement</a
+        >.
+    </p>
+</div>
+
 <h2>top albums i'm listening</h2>
 <div class="grid grid-cols-3 lg:grid-cols-5 gap-1 mb-14">
     {#each data.myTopAlbums.slice(0, 5) as { name, artist, image, albumLink, artistLink }}
