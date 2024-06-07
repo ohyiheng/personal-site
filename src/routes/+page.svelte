c0ce5673b960ed4e2b00aaa924745c77bdf4c3ef
Yi Heng <rainmrn@proton.me>
2024-06-07 14:50:13 +0800

    refactor: Use better spacing tailwind classes and remove redundant lorem ipsum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,62 +3,27 @@
 </script>
 
 <Title title={"hey there! i'm yiheng"} />
-<div class="text-left">
-    <p class="mb-3">
+<div class="text-left space-y-3 mb-12">
+    <p>
         Ducimus quia quia eum. Illum odio et rerum ab et. Accusamus maxime quasi
         dolorem. A voluptates dolor aut qui facere ullam. Qui doloribus
         voluptatem odio consectetur exercitationem. Est voluptas dolorem a
         soluta.
     </p>
-    <p class="mb-3">
+    <p>
         Vitae amet sed porro tempore eum debitis repellat doloribus. Sit fugit
         impedit deleniti laboriosam in quae velit. Placeat fugiat qui dicta
         mollitia modi corporis molestiae ea. Aliquam sunt sunt aliquam est.
         Corporis nulla adipisci iste cumque reiciendis.
     </p>
-    <p class="mb-3">
+    <p>
         Consequatur illum sint voluptatum commodi. Sunt corrupti corporis ipsam
         occaecati nesciunt molestiae. Alias adipisci quibusdam enim
         exercitationem enim vel laborum. Accusantium vero ut et occaecati dicta
         quasi minima. Quis architecto eum velit molestiae vel. Qui sit et quia
         illum esse quibusdam.
     </p>
-    <p class="mb-3">
-        Consequatur illum sint voluptatum commodi. Sunt corrupti corporis ipsam
-        occaecati nesciunt molestiae. Alias adipisci quibusdam enim
-        exercitationem enim vel laborum. Accusantium vero ut et occaecati dicta
-        quasi minima. Quis architecto eum velit molestiae vel. Qui sit et quia
-        illum esse quibusdam.
-    </p>
-    <p class="mb-3">
-        Consequatur illum sint voluptatum commodi. Sunt corrupti corporis ipsam
-        occaecati nesciunt molestiae. Alias adipisci quibusdam enim
-        exercitationem enim vel laborum. Accusantium vero ut et occaecati dicta
-        quasi minima. Quis architecto eum velit molestiae vel. Qui sit et quia
-        illum esse quibusdam.
-    </p>
-    <p class="mb-3">
-        Consequatur illum sint voluptatum commodi. Sunt corrupti corporis ipsam
-        occaecati nesciunt molestiae. Alias adipisci quibusdam enim
-        exercitationem enim vel laborum. Accusantium vero ut et occaecati dicta
-        quasi minima. Quis architecto eum velit molestiae vel. Qui sit et quia
-        illum esse quibusdam.
-    </p>
-    <p class="mb-3">
-        Consequatur illum sint voluptatum commodi. Sunt corrupti corporis ipsam
-        occaecati nesciunt molestiae. Alias adipisci quibusdam enim
-        exercitationem enim vel laborum. Accusantium vero ut et occaecati dicta
-        quasi minima. Quis architecto eum velit molestiae vel. Qui sit et quia
-        illum esse quibusdam.
-    </p>
-    <p class="mb-3">
-        Consequatur illum sint voluptatum commodi. Sunt corrupti corporis ipsam
-        occaecati nesciunt molestiae. Alias adipisci quibusdam enim
-        exercitationem enim vel laborum. Accusantium vero ut et occaecati dicta
-        quasi minima. Quis architecto eum velit molestiae vel. Qui sit et quia
-        illum esse quibusdam.
-    </p>
-    <p class="mb-12">
+    <p>
         Assumenda enim distinctio dignissimos sed asperiores doloremque adipisci
         similique. Officia voluptate id at quo. Ex omnis ut ea. Expedita quo
         sint sit quaerat.
