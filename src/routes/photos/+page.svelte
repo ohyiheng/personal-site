cc06e6e96308a52107bf12f7d99d4a703cf7fb52
Yi Heng <rainmrn@proton.me>
2024-06-08 17:34:38 +0800

    style: Add macy library for masonry-like grid layout in photos page

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,14 +1,40 @@
 <script>
     import Title from "$lib/Title.svelte";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        const Macy = (await import("macy")).default;
+        var macy = new Macy({
+            container: "#photos-container",
+            margin: 16,
+            breakAt: {
+                1280: 4,
+                1024: 3,
+                768: 2,
+            },
+        });
+    });
 </script>
 
-<Title title={"photos i took"} />
+<h1>photos i took</h1>
 
-<div>
-    <img src="/alcohol.jpg" alt="">
-    <img src="/avocado.jpg" alt="">
-    <img src="/block.jpg" alt="">
-    <img src="/mountain.jpg" alt="">
-    <img src="/plant.jpg" alt="">
-    <img src="/shake.jpg" alt="">
+<div class="grid" id="photos-container">
+    <img src="/alcohol.jpg" alt="" />
+    <img src="/avocado.jpg" alt="" />
+    <img src="/block.jpg" alt="" />
+    <img src="/mountain.jpg" alt="" />
+    <img src="/plant.jpg" alt="" />
+    <img src="/shake.jpg" alt="" />
+    <img src="/alcohol.jpg" alt="" />
+    <img src="/avocado.jpg" alt="" />
+    <img src="/block.jpg" alt="" />
+    <img src="/mountain.jpg" alt="" />
+    <img src="/plant.jpg" alt="" />
+    <img src="/shake.jpg" alt="" />
+    <img src="/alcohol.jpg" alt="" />
+    <img src="/avocado.jpg" alt="" />
+    <img src="/block.jpg" alt="" />
+    <img src="/mountain.jpg" alt="" />
+    <img src="/plant.jpg" alt="" />
+    <img src="/shake.jpg" alt="" />
 </div>
