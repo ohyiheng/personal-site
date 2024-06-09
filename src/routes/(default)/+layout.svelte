f7e3cdb11caf18d32e9dbff1e44f5f078b928d09
Yi Heng <rainmrn@proton.me>
2024-06-09 13:25:25 +0800

    refactor: :art: Group common layout components to remove boilerplate.

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -1,12 +1,3 @@
-<script>
-    import NavBar from "$lib/NavBar.svelte";
-    import "/src/app.css";
-</script>
-
-<NavBar />
-
-<hr class="border-gray-300 dark:border-gray-700 mb-16" />
-
 <main class="mx-auto w-[90vw] md:w-[65ch]">
     <slot />
 </main>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import NavBar from "$lib/NavBar.svelte";
+    import "/src/app.css";
+</script>
+
+<NavBar />
+
+<hr class="border-gray-300 dark:border-gray-700 mb-16" />
+
+<slot />
