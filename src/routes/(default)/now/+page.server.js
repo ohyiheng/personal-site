3c4182934e393beaa4fba7797b7f94b8b821903c
Yi Heng <rainmrn@proton.me>
2024-06-09 13:08:03 +0800

    feat: :sparkles: Integrate Sanity CMS for /now page updates
    Fetch /now content from Sanity CMS, use a third party package to convert PortableText to HTML.
    
    BREAKING CHANGE: Only supports Sanity CMS, additional modifications are probably needed for other CMS.

--- src/routes/(default)/now/+page.server.js
+++ src/routes/(default)/now/+page.server.js
@@ -1,6 +1,8 @@
 export async function load() {
     const lastFMAPIKey = import.meta.env.VITE_LASTFM_API_KEY;
     const lastFMUsername = import.meta.env.VITE_LASTFM_USERNAME;
+    const sanityToken = import.meta.env.VITE_SANITY_TOKEN;
+    const queryURL = import.meta.env.VITE_SANITY_HTTP_QUERY_NOW
 
     async function fetchLastFM(method, period) {
         try {
@@ -8,7 +10,7 @@ export async function load() {
                 `https://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastFMUsername}&period=${period}&api_key=${lastFMAPIKey}&format=json`,
             );
             if (!response.ok) {
-                throw new Error(`Error with status: ${response.status}!`);
+                throw new Error(`Fetch last.fm failed with status: ${response.status}!`);
             }
             const data = await response.json();
             return data;
@@ -17,17 +19,38 @@ export async function load() {
         }
     }
 
-    const topAlbums = await fetchLastFM("user.getTopAlbums", "7day");
+    async function fetchNowUpdates() {
+        try {
+            const response = await fetch(queryURL, {
+                headers: {
+                    'Authorization': `Bearer ${sanityToken}`
+                }
+            });
+            if (!response.ok) {
+                throw new Error(`Fetch now updates failed with status: ${response.status}!`);
+            }
+            return await response.json();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const topAlbumsFetch = await fetchLastFM("user.getTopAlbums", "7day");
+    const nowUpdatesFetch = await fetchNowUpdates();
     // const topTracks = fetchLastFM("user.getTopTracks", "7day");
 
     return {
-        myTopAlbums: topAlbums.topalbums.album.map((album) => ({
+        topAlbums: topAlbumsFetch.topalbums.album.map((album) => ({
             name: album.name,
             artist: album.artist.name,
             image: album.image[2]['#text'],
             albumLink: album.url,
             artistLink: album.artist.url
-        }))
+        })),
+        nowUpdates: {
+            date: nowUpdatesFetch.result[0].date,
+            content: nowUpdatesFetch.result[0].updates
+        }
     }
 }
 
--- src/routes/(default)/now/+page.svelte
+++ src/routes/(default)/now/+page.svelte
@@ -1,25 +1,14 @@
 <script>
     export let data;
+    import { PortableText } from '@portabletext/svelte';
 </script>
 
 <h1>what i'm doing now</h1>
 
 <div class="space-y-4 mb-14">
-    <p>
-        I'm currently on a sembreak, chilling at home. My next semester is going
-        to be a lot harder than the first two, as I'm about to dive into
-        unfamiliar territory, stuff like Discrete Math, Computer Architecture,
-        more advanced Data Structures.
-    </p>
-    <p>
-        They seem exciting, but I'm also worried about them taking up too much
-        of my focus. On one hand I do want to direct most of my focus on the
-        courses, but on the other hand, I would also like to spend more time
-        doing other stuff, maybe joining clubs and societies, or taking up a
-        personal project.
-    </p>
-    <p class="italic">
-        Last updated: 2024-06-06.<br /> This page is inspired by the
+    <PortableText value={data.nowUpdates.content} />
+    <p class="italic text-caption">
+        Last updated: {data.nowUpdates.date}.<br /> This page is inspired by the
         <a href="https://nownownow.com/about" target="_blank"
             >/now page movement</a
         >.
@@ -28,7 +17,7 @@
 
 <h2>top albums i'm listening</h2>
 <div class="grid grid-cols-3 lg:grid-cols-5 gap-1 mb-14">
-    {#each data.myTopAlbums.slice(0, 5) as { name, artist, image, albumLink, artistLink }}
+    {#each data.topAlbums.slice(0, 5) as { name, artist, image, albumLink, artistLink }}
         <a
             href={albumLink}
             target="_blank"
