671a5c03b2c2a50046aff52051c712756af4966c
Yi Heng <rainmrn@proton.me>
2024-06-09 13:21:44 +0800

    style: :lipstick: Give navbar its own background
    This is so that I can potentially implement a sticky navbar in the future.

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -3,15 +3,17 @@
     import NavButton from "$lib/NavButton.svelte";
 </script>
 
-<nav class="text-center w-11/12 mx-auto mb-8 md:w-4/5 xl:w-3/5">
-    <a href="/" class="w-36 h-auto inline-block mx-auto mt-6 mb-5">
-        <Logo />
-    </a>
-
-    <div class="flex flex-wrap gap-3">
-        <NavButton label={"about"} />
-        <NavButton label={"now"} />
-        <NavButton label={"photos"} />
-        <NavButton label={"uses"} />
-    </div>
-</nav>
+<div class="bg-white-100 dark:bg-gray-850">
+    <nav class="text-center w-11/12 pb-8 mx-auto md:w-4/5 xl:w-3/5">
+        <a href="/" class="w-36 h-auto inline-block mx-auto mt-6 mb-5">
+            <Logo />
+        </a>
+    
+        <div class="flex flex-wrap gap-3">
+            <NavButton label={"about"} />
+            <NavButton label={"now"} />
+            <NavButton label={"photos"} />
+            <NavButton label={"uses"} />
+        </div>
+    </nav>
+</div>
