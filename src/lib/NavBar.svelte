0bd830a4431c686acc23442e23c031da0af1f8de
Yi Heng <rainmrn@proton.me>
2024-06-11 14:52:56 +0800

    style: :lipstick: Add a custom error page that uses a different logo variation

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,12 +1,18 @@
 <script>
     import Logo from "$lib/Logo.svelte";
+    import LogoError from "$lib/LogoError.svelte";
     import NavButton from "$lib/NavButton.svelte";
+    import { page } from '$app/stores';
 </script>
 
 <div class="bg-white-100 dark:bg-gray-850">
     <nav class="text-center w-11/12 pb-8 mx-auto md:w-4/5 xl:w-3/5">
         <a href="/" class="w-24 sm:w-28 md:w-32 h-auto inline-block mx-auto mt-6 mb-5">
+            {#if $page.error}
+            <LogoError />
+            {:else}
             <Logo />
+            {/if}
         </a>
     
         <div class="flex flex-wrap gap-3">
