b01fcc4b9e4f5a73be9fc55baf1cd1a8c557c73f
Yi Heng <rainmrn@proton.me>
2024-06-14 23:33:06 +0800

    perf: :children_crossing: Make /now page appear sooner by fetching last.fm data only after the page has loaded

--- src/routes/(default)/now/+page.server.js
+++ src/routes/(default)/now/+page.server.js
@@ -1,36 +1,12 @@
 export async function load(event) {
-    const lastFMAPIKey = import.meta.env.VITE_LASTFM_API_KEY;
-    const lastFMUsername = import.meta.env.VITE_LASTFM_USERNAME;
-
-    async function fetchLastFM(method, period) {
-        try {
-            const response = await fetch(
-                `https://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastFMUsername}&period=${period}&api_key=${lastFMAPIKey}&format=json`,
-            );
-            if (!response.ok) {
-                throw new Error(`Fetch last.fm failed with status: ${response.status}!`);
-            }
-            const data = await response.json();
-            return data;
-        } catch (err) {
-            console.error("Error:", err);
-        }
-    }
+    
 
     const nowUpdateData = await event.locals.sanityClient
         .fetch("*[_type == 'nowUpdate'] | order(_createdAt desc)[0]")
         .catch((err) => { console.log("Fetch now update failed: ", err.message) });
 
-    const topAlbumsFetch = await fetchLastFM("user.getTopAlbums", "7day");
-
     return {
-        topAlbums: topAlbumsFetch.topalbums.album.map((album) => ({
-            name: album.name,
-            artist: album.artist.name,
-            image: album.image[2]['#text'],
-            albumLink: album.url,
-            artistLink: album.artist.url
-        })),
+        title: "what i'm currently doing",
         nowUpdate: { date: nowUpdateData.date, content: nowUpdateData.updates }
     }
 }
--- src/routes/(default)/now/+page.svelte
+++ src/routes/(default)/now/+page.svelte
@@ -1,47 +1,63 @@
 <script>
     export let data;
-    import { PortableText } from '@portabletext/svelte';
+    import { PortableText } from "@portabletext/svelte";
+    import Link from "$lib/Link.svelte";
+
+    const lastFMAPIKey = import.meta.env.VITE_LASTFM_API_KEY;
+    const lastFMUsername = import.meta.env.VITE_LASTFM_USERNAME;
+
+    async function fetchLastFM(method, period) {
+        try {
+            const response = await fetch(
+                `https://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastFMUsername}&period=${period}&api_key=${lastFMAPIKey}&format=json`,
+            );
+            if (!response.ok) {
+                throw new Error(
+                    `Fetch last.fm failed with status: ${response.status}!`,
+                );
+            }
+            const data = await response.json();
+            console.log(data.topalbums.album[0]);
+            return data.topalbums.album;
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
 </script>
 
-<h1>what i'm doing now</h1>
-
-<div class="space-y-4 mb-14">
+<div class="space-y-4 pb-20">
     <PortableText value={data.nowUpdate.content} />
-    <p class="italic text-caption">
-        Last updated: {data.nowUpdate.date}.<br /> This page is inspired by the
-        <a href="https://nownownow.com/about" target="_blank"
-            >/now page movement</a
-        >.
+    <p class="text-caption">
+        Last updated: <span
+            class="inline-block px-1 rounded-md bg-secondary-100 text-secondary-600 dark:bg-secondary-600 dark:text-secondary-100 font-bold"
+            >{data.nowUpdate.date}</span
+        ><br /> This page is inspired by the
+        <Link href={"https://nownownow.com/about"}>/now page movement</Link>.
     </p>
 </div>
 
 <h2>top albums i'm listening</h2>
-<div class="grid grid-cols-3 lg:grid-cols-5 gap-1 mb-14">
-    {#each data.topAlbums.slice(0, 5) as { name, artist, image, albumLink, artistLink }}
-        <a
-            href={albumLink}
-            target="_blank"
-            class="px-2 py-3 rounded-sm hover:bg-primary-600/10 dark:hover:bg-primary-100/10"
-        >
-            <img
-                src={image}
-                alt="Album image of {name}"
-                class="w-full object-cover rounded shadow-md shadow-primary-700/40 dark:shadow-primary-900 mb-3"
-            />
-            <div class="flex flex-col truncate">
-                <a
-                    href={albumLink}
-                    target="_blank"
-                    class="truncate text-base hover:text-secondary-500 dark:hover:text-secondary-400"
-                    >{name}</a
-                >
-                <a
-                    href={artistLink}
-                    target="_blank"
-                    class="text-primary-600 text-sm truncate hover:text-secondary-500 dark:hover:text-secondary-400"
-                    >{artist}</a
-                >
-            </div>
-        </a>
-    {/each}
-</div>
+{#await fetchLastFM("user.getTopAlbums", "7day")}
+    <div class="flex items-center justify-center">
+        <p class="mt-4 inline-block">Loading album listening stats...</p>
+        <div
+            class="w-5 h-5 p-0 rounded-full border-[3px] bg-transparent border-gray-600 border-t-secondary-400 animate-spin inline-block ml-4"
+        ></div>
+    </div>
+{:then albums}
+    <div class="grid grid-cols-3 gap-3">
+        {#each albums.slice(0, 6) as album}
+            <a href={album.url} target="_blank">
+                <img
+                    src={album.image[2]["#text"]}
+                    alt="Album image of {album.name}"
+                    class="w-full object-cover rounded border border-gray-200 dark:border-gray-850 hover:shadow-lg shadow-gray-200 dark:shadow-gray-950 mb-3 hover:scale-105 hover:-rotate-3 duration-150 ease-in-out"
+                />
+            </a>
+        {/each}
+    </div>
+{:catch err}
+    <p class="text-red-400">
+        Error loading music listening stats: {err.message}
+    </p>
+{/await}
