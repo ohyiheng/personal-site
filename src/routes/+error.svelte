a1c5ef6d3ff0f66a52769b510826c411b2f30acf
Yi Heng <rainmrn@proton.me>
2024-06-14 23:22:42 +0800

    style: :lipstick: Make error page look a little bit better

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,4 +2,10 @@
 	import { page } from "$app/stores";
 </script>
 
-<h1>{$page.status}: {$page.error.message}</h1>
+<div class="flex flex-col flex-grow">
+	<div
+		class="w-[90vw] max-w-[54rem] mx-auto md:border-x md:px-8 pt-12 pb-14 border-gray-200 dark:border-gray-700 flex-grow"
+	>
+		<h1 class="text-center">{$page.status}: {$page.error.message}</h1>
+	</div>
+</div>
