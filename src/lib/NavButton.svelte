e4f4cea28a42e1524353f5cdfae53a3ddbe3c3ce
Yi Heng <rainmrn@proton.me>
2024-07-21 20:27:15 +0800

    style: Make nav bar cleaner
    Remove rotation when hovered over, remove underline indicating the page a user is on, change the text color when hovered over instead

--- src/lib/NavButton.svelte
+++ src/lib/NavButton.svelte
@@ -6,12 +6,9 @@
 <a
     href="/{label}"
     class="text-base font-medium font-display border-b-2 border-transparent
-        text-gray-750 dark:text-gray-150 hover:text-secondary-500 dark:hover:text-secondary-400 dark:hover:border-secondary-400
+        text-gray-750 dark:text-gray-150 dark:hover:text-secondary-400
            flex-1
            hover:cursor-pointer
-           aria-[current=true]:border-secondary-500 dark:aria-[current=true]:border-secondary-400
-           hover:border-b-2 hover:border-secondary-500 
-           transition-transform duration-200 ease-out
-           hover:-rotate-3"
+           hover:border-b-2 hover:text-secondary-50 duration-100 ease-out"
     aria-current={$page.url.pathname === `/${label}`}>{label}</a
 >
